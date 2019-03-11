403901f614b4f79b8fa297a77d7a205a5a0872cf
Odin Aleksander Severinsen <odin@severinsen.no>
2019-03-11 20:52:02 +0100

    Changed subplot to something that works

--- @Plotex/subplot.m
+++ @Plotex/subplot.m
@@ -1,16 +1,35 @@
-function [fig, subp] = subplot_nice(data, labels, font_size, varargin)
+function subplot(obj, varargin)
 
-    [rows, cols] = size(data);
+    [rows, cols] = size(obj.data);
     subp = zeros(rows, cols);
 
-    fig = figure;
+    obj.fig = figure;
+    
+    valid_modes = {'plot', 'loglog', 'stairs'};
+    
+    if ~isempty(varargin)
+        
+        modes = varargin{1};
+    
+        if any(size(modes) ~= [rows, cols])
+            error('Modes cell array must be same dimension as data cell array.');
+        end
+        
+        for i = 1:length(varargin)
+            validatestring(modes{i}, valid_modes); 
+        end
+    end
     
     for i = 1:rows
         for j = 1:cols
             subp(i, j) = subplot(rows, cols, (i-1)*cols + j);
-            plot_nice(data{i, j}, labels{i, j}, font_size, varargin{:}, 'disableFigure', fig);
+            D = Plotex(obj.data(i, j));
+            D.disable('figure');
+            if ~isempty(varargin) && ~strcmp(modes{i, j}, 'plot')
+                D.enable(modes{i, j});
+            end
+            D.plot();
         end
     end
     
 end
-
